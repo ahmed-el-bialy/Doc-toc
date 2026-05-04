00ff613b9f6b8d7bdaee7aef6f6aab255d28837a
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-05-04 19:38:28 +0300

    add headline to onboarding screen and add text style to theming folder

---
+++ lib/core/theming/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextStyles {
+  static TextStyle font24Black700Weight = TextStyle(
+    fontSize: 30.sp,
+    fontWeight: FontWeight.w700,
+  );
+}
--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -1,10 +1,23 @@
+import 'package:doc_tok/features/onboarding/widgets/doc_logo_and_name.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Card(child: Text("Hello")));
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30, bottom: 30).h,
+            child: Column(
+                mainAxisAlignment:MainAxisAlignment.center,
+                children: [DocLogoAndName()]),
+          ),
+        ),
+      ),
+    );
   }
 }
